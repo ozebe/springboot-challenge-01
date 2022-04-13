3429760bdbfb778540a3166d515ff28927a71f20
Wesley Ozebe <wesleysantos345@hotmail.com>
2022-04-13 01:16:41 -0300

    API - Finalizado Model de Pessoa

--- src/main/java/com/teste/demo/model/Pessoa.java
+++ src/main/java/com/teste/demo/model/Pessoa.java
@@ -1,8 +1,124 @@
 package com.teste.demo.model;
 
+
+import java.util.Date;
+import java.util.Objects;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
+@Table(name = "pessoa")
 public class Pessoa {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(nullable = false, length = 255)
+	private String nome;
+	
+	
+	private Date data_nasc;
+	
+	@Column(nullable = false, length = 11, unique = true)
+	private String cpf;
+	
+	
+	private char sexo;
+	
+	private Double altura;
+	
+	private Double peso;
 
+	public Pessoa() {
+	}
+
+	public Pessoa(String nome, Date data_nasc, String cpf, char sexo, Double altura, Double peso) {
+		this.nome = nome;
+		this.data_nasc = data_nasc;
+		this.cpf = cpf;
+		this.sexo = sexo;
+		this.altura = altura;
+		this.peso = peso;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Date getData_nasc() {
+		return data_nasc;
+	}
+
+	public void setData_nasc(Date data_nasc) {
+		this.data_nasc = data_nasc;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public char getSexo() {
+		return sexo;
+	}
+
+	public void setSexo(char sexo) {
+		this.sexo = sexo;
+	}
+
+	public Double getAltura() {
+		return altura;
+	}
+
+	public void setAltura(Double altura) {
+		this.altura = altura;
+	}
+
+	public Double getPeso() {
+		return peso;
+	}
+
+	public void setPeso(Double peso) {
+		this.peso = peso;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(cpf);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Pessoa other = (Pessoa) obj;
+		return Objects.equals(cpf, other.cpf);
+	}
+
+	
+	
 }
