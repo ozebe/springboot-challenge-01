1c51d035585e160577601362a0a0ce32d21a86f3
Wesley Ozebe <33488578+ozebe@users.noreply.github.com>
2022-04-13 09:28:25 -0300

    adicionado OpenAPI

--- src/main/java/com/teste/demo/PessoasApplication.java
+++ src/main/java/com/teste/demo/PessoasApplication.java
@@ -3,9 +3,11 @@ package com.teste.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 
 @SpringBootApplication
 @EnableSpringDataWebSupport
+@OpenAPIDefinition
 public class PessoasApplication {
 
 	public static void main(String[] args) {
